56f44c9a3ab7de85982b51de0aa19a604528f071
stereobooster <stereobooster@gmail.com>
2023-10-01 17:43:16 +0200

    experiment

--- .github/get_target.js
+++ .github/get_target.js
@@ -1,3 +1,10 @@
 import { getTarget } from "../index.js";
 
+process.stderr.write(
+  JSON.stringify({
+    platform: process.platform,
+    arch: process.arch,
+  })
+);
+process.stderr.write("\n");
 process.stdout.write(getTarget(process));
