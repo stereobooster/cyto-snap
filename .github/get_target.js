6d91a43cbb5d227160a149958caba2eac50bcb28
stereobooster <stereobooster@gmail.com>
2023-10-01 17:49:13 +0200

    experiment

--- .github/get_target.js
+++ .github/get_target.js
@@ -1,10 +1,3 @@
 import { getTarget } from "../index.js";
-
-process.stderr.write(
-  JSON.stringify({
-    platform: process.platform,
-    arch: process.arch,
-  })
-);
-process.stderr.write("\n");
-process.stdout.write(getTarget(process));
+import { arch, platform } from "node:process";
+process.stdout.write(getTarget({ arch, platform }));
