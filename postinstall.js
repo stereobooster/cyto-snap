a5db8a69d4bb81692ba617fd02fa3f7a031c9349
stereobooster <stereobooster@gmail.com>
2023-10-04 19:43:04 +0200

    Fix for postinstall

--- postinstall.js
+++ postinstall.js
@@ -1,18 +1,24 @@
 import { arch, platform, exit } from "node:process";
 import { existsSync, copyFileSync } from "node:fs";
 import { resolve, dirname } from "node:path";
-import { fileURLToPath } from 'node:url';
+import { fileURLToPath } from "node:url";
 import { executablePath } from "./index.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const path = resolve(__dirname, executablePath({ arch, platform }));
+try {
+  // import any of devDependencies
+  await import("odiff-bin");
+  // if ok we can assume this is development mode
+} catch (e) {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const path = resolve(__dirname, executablePath({ arch, platform }));
 
-console.log(path)
+  if (!existsSync(path)) {
+    console.log(
+      "There is no precompiled binary for your OS/architecture. Sorry"
+    );
+    exit(1);
+  }
 
-if (!existsSync(path)) {
-  console.log("There is no precompiled binary for your OS/architecture. Sorry");
-  exit(1);
+  copyFileSync(path, resolve(__dirname, "bin/cyto-snap"));
 }
-
-copyFileSync(path, resolve(__dirname, "bin/cyto-snap"));
